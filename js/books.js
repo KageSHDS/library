677763e67ae48e682e2d7676a2284c4a1019bdb5
KageSHDS <RabITquestion@yandex.ru>
2024-09-10 22:35:36 +0300

    final

--- js/books.js
+++ js/books.js
@@ -1,162 +1,55 @@
-// const books = JSON.parse(localStorage.getItem('books')) || [];
 
-// // const book = {};
-// // book.title = "Frontend для чайников";
-// // book.author = "John White";
-// // book.year = 2022;
-// // book.genre = "Техническая литература";
-// // book.status = "прочитано";
-
-// // books.push(book);
-// // books.push(book);
-// // books.push(book);
-// // books.push(book);
-// // books.push(book);
-// // books.push(book);
-
-
-// const book_list = document.getElementById('list_books');
-
-// displayBooks();
-
-// function indexBook(book) {
-//     const bookNumber = books.indexOf(book);
-//     return bookNumber;
-// }
-
-// function displayBooks(){
-//     book_list.innerHTML = ``;
-//     books.forEach(displayBook);
-// };
-
-// function saveBooks(){
-//     localStorage.setItem('books', JSON.stringify(books));
-// }
-
-// function displayBook(book){
-//     const bookNumber = books.indexOf(book);
-//     book_list.innerHTML += `
-//     <div class="book">
-//         <h2>${book.title}</h2>
-//         <p>${book.author}</p>
-//         <p>${book.year}</p>
-//         <p>${book.genre}</p>
-//         <p>${book.status}</p>
-//         <button onclick = "deleteBook(${bookNumber})">Удалить</button>
-//         <button onclick = "changeBook(${bookNumber})">Изменить</button>
-//     </div>
-//     `
-// }
-
-// function addBook() {
-//     const book = {};
-// 	book.title = document.getElementById('title').value;
-// 	book.author = document.getElementById('author').value;
-// 	book.year = document.getElementById('years').value;
-// 	book.genre = document.getElementById('genre').value;
-// 	book.status = document.getElementById('status').value;
-//     // console.log(indexBook(book));
-//     document.forms[0].reset()
-//     // books[0] = book
-//     books.push(book);
-//     displayBooks();
-//     saveBooks();
-//     return false;
-// }
-
-// function deleteBook(bookNumber) {
-//     books.splice(bookNumber, 1);
-//     saveBooks();
-//     displayBooks();
-// }
-
-// function changeBook(bookNumber) {
-//     document.forms[0].reset();
-//     const book = books[bookNumber];
-// 	document.getElementById('title').value = book.title;
-// 	document.getElementById('author').value = book.author;
-// 	document.getElementById('years').value = book.year;
-// 	document.getElementById('genre').value = book.genre;
-// 	document.getElementById('status').value = book.status;
-//     console.log(bookNumber);
-//     saveBooks();
-//     displayBooks();
-// }
-
-// function appleChangeBook(){
-//     console.log("worked");
-//     const book = {};
-// 	book.title = document.getElementById('title').value;
-// 	book.author = document.getElementById('author').value;
-// 	book.year = document.getElementById('years').value;
-// 	book.genre = document.getElementById('genre').value;
-// 	book.status = document.getElementById('status').value;
-//     books[1] = book;
-//     saveBooks();
-//     displayBooks();
-//     return false;
-// }
-
-// let text;
-// function quantityBook() {
-//     text = document.getElementById('footer_number');
-//     text.textContent = `кол-во книг: ${books.length}`
-//     return text;
-// }
-// quantityBook();
-
-
-function uploadPage() {
+function uploadPage() { //* функция позволяющая обновить страницу
     window.location.reload();
 }
 
-function deletedAllBook(){
+function deletedAllBook() { //* функция которая удаляет полностью все книги на странице 
     books.length = 0;
     saveToLocalStorage();
 	displayBook();
 };
 
 let currentEditItem = null;
-let books = JSON.parse(localStorage.getItem('books')) || []
+let books = JSON.parse(localStorage.getItem('books')) || []      
 
-function saveToLocalStorage() {
+function saveToLocalStorage() { //* сохраняем данные в local storage, для того чтобы данные после обновления страницы сохранялись 
 	localStorage.setItem('books', JSON.stringify(books));
 };
 
-function displayBook() {
-	const list = document.getElementById('list_books');
-    const filterStatus = document.getElementById("filterStatus").value;
+function displayBook() { //* функция для вывода книг на экран
+	const list = document.getElementById('list_books');  //* присваиваем list объект list_books
+    const filterStatus = document.getElementById("filterStatus").value; //* создаём переменную для проверки статуса фильтра
 	list.innerHTML = '';
     
-	books.forEach((book, index) => {
-        if(filterStatus === "All" || book.status === filterStatus){
-		    const listItem = document.createElement('li');
+	books.forEach((book, index) => { //* перебираем каждый элемент
+        if(filterStatus === "All" || book.status === filterStatus){ //* проверка фильтра статуса 
+		    const listItem = document.createElement('li'); //* создаём переменные для занесения данных введенных параметров
 		    const bookDiv = document.createElement('div');
 		    bookDiv.classList.add('bookItem');
 
 		    const titleText = document.createElement('div');
 		    titleText.classList.add('title');
-		    titleText.textContent = `Title: ${book.title}`;
+		    titleText.textContent = `Название: ${book.title}`
 
 		    const authorText = document.createElement('div');
 		    authorText.classList.add('author');
-		    authorText.textContent = `Author: ${book.author}`;
+		    authorText.textContent = `Автор: ${book.author}`
 
 		    const yearText = document.createElement('div');
 		    yearText.classList.add('year');
-		    yearText.textContent = `year: ${book.year}`;
+		    yearText.textContent = `Год: ${book.year}`
 
 		    const genreText = document.createElement('div');
 		    genreText.classList.add('genre');
-		    genreText.textContent = `genre: ${book.genre}`;
+		    genreText.textContent = `Жанр: ${book.genre}`
 
 		    const statusText = document.createElement('div');
 		    statusText.classList.add('status');
-		    statusText.textContent = `status: ${book.status}`;
+		    statusText.textContent = `Статус: ${book.status}`;
 
 		    const editBook = document.createElement('button');
 		    editBook.textContent = 'Изменить';
-		    editBook.addEventListener('click', function () {
+		    editBook.addEventListener('click', function () {  //* создаём обработчик событий на кнопке изменить 
 			    currentEditItem = index;
 			    document.getElementById('title').value = book.title;
 			    document.getElementById('author').value = book.author;
@@ -173,7 +66,7 @@ function displayBook() {
 			    displayBook();
 		    });
 
-		    bookDiv.appendChild(titleText);
+		    bookDiv.appendChild(titleText); //* добавляем значения и заносим их в список
 		    bookDiv.appendChild(authorText);
             bookDiv.appendChild(yearText);
             bookDiv.appendChild(genreText);
@@ -186,11 +79,11 @@ function displayBook() {
 	});
 }
 
-document.getElementById('add_form').addEventListener('submit', function (event) {
+document.getElementById('add_form').addEventListener('submit', function (event) { //* добавление книг
 		
         event.preventDefault();
 
-		const title = document.getElementById('title').value;
+		const title = document.getElementById('title').value; //* фиксируем введенные значения
 		const author = document.getElementById('author').value;
 		const year = document.getElementById('year').value;
 		const genre = document.getElementById('genre').value;
@@ -208,18 +101,18 @@ document.getElementById('add_form').addEventListener('submit', function (event) 
 		saveToLocalStorage();
 		displayBook();
 
-		document.getElementById('title').value = '';
+		document.getElementById('title').value = ''; //* обнуляем значения 
 		document.getElementById('author').value = '';
 		document.getElementById('year').value = '';
 		document.getElementById('genre').value = '';
-		document.getElementById('status').value = 'unread';
+		document.getElementById('status').value = 'Не прочитано';
 	});
 
 displayBook();
 
 
 let text
-function quantityBook() {
+function quantityBook() { //* показывает кол-во книг на странице
 	text = document.getElementById('footer_number');
 	text.textContent = `кол-во книг: ${books.length}`;
 }
